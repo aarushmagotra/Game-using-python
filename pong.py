4b3948507d14dda1bc7e5cb105ed36cda97bf2a0
muteenk <kundangarmuteen@protonmail.com>
2020-12-01 19:53:33 +0530

    made walls for bouncing ball

--- pong.py
+++ pong.py
@@ -1,5 +1,10 @@
+###################
+# IMPORTED MODULES
+###################
+
 import turtle as tr
-import tkinter as tk
+import random as r
+
 
 ########################
 #   OBJECTS ON SCREEN
@@ -12,10 +17,23 @@ win.setup(height=800, width=1000)
 win.title("Pong")
 win.tracer(0)
 
-# left paddle 
+# For Full Screen
+box = tr.Turtle("classic")
+box.hideturtle()
+box.color("white")
+box.penup()
+box.goto(-500, -400)
+box.pendown()
+box.goto(500, -400)
+box.goto(500, 400)
+box.goto(-500, 400)
+box.goto(-500, -400)
+
+
+# left paddle
 lPaddle = tr.Turtle("square")
 lPaddle.color("blue")
-lPaddle.shapesize(6, 1.3)
+lPaddle.shapesize(6, 1)
 lPaddle.speed(0)
 lPaddle.penup()
 lPaddle.goto(-450, 0)
@@ -23,7 +41,7 @@ lPaddle.goto(-450, 0)
 # right paddle
 rPaddle = tr.Turtle("square")
 rPaddle.color("blue")
-rPaddle.shapesize(6, 1.3)
+rPaddle.shapesize(6, 1)
 rPaddle.speed(0)
 rPaddle.penup()
 rPaddle.goto(450, 0)
@@ -32,8 +50,14 @@ rPaddle.goto(450, 0)
 ball = tr.Turtle("circle")
 ball.shapesize(1.2, 1.2)
 ball.color("red")
+ball.speed(1)
 ball.penup()
-    
+ball_dir = r.choice([-150, -60, -45, -30, 30, 45, 60, 150])
+ballx = r.choice([0.5, -0.5])
+bally = r.choice([0.5, -0.5])
+# ball.setx(-428)
+# ball.sety(0)
+# ball.left(ball_dir)
 
 
 #######################
@@ -42,24 +66,40 @@ ball.penup()
 
 def rightUp():
     rightY = rPaddle.ycor()
-    if(rightY <= 350):
-        rPaddle.sety(rightY+20)
+    if(rightY <= 328):
+        rPaddle.sety(rightY+30)
+
 
 def rightDown():
     rightY = rPaddle.ycor()
-    if(rightY >= -350):
-        rPaddle.sety(rightY-20)
+    if(rightY >= -328):
+        rPaddle.sety(rightY-30)
+
 
 def leftUp():
     leftY = lPaddle.ycor()
-    if(leftY <= 350):
-        lPaddle.sety(leftY+20)
+    if(leftY <= 328):
+        lPaddle.sety(leftY+30)
+
 
 def leftDown():
     leftY = lPaddle.ycor()
-    if(leftY >= -350):
-        lPaddle.sety(leftY-20)
+    if(leftY >= -328):
+        lPaddle.sety(leftY-30)
 
+
+def bounceX():
+    global ballx
+    ball.shapesize(0.8, 1.2)
+    ballx *= -1
+    ball.shapesize(1.2, 1.2)
+
+
+def bounceY():
+    global bally
+    ball.shapesize(1.2, 0.8)
+    bally *= -1
+    ball.shapesize(1.2, 1.2)
 
 
 #######################
@@ -77,4 +117,30 @@ win.onkeypress(leftDown, "s")
 while True:
     win.update()
 
-    
+    # Border Collision
+    if ball.ycor() > 388:
+        ball.sety(388)
+        bounceY()
+
+    if ball.ycor() < -388:
+        ball.sety(-388)
+        bounceY()
+
+    # if ball.xcor() < 485:
+    #     # ball.goto(0, 0)
+    #     bounceX()
+
+    # if ball.xcor() < -485:
+    #     # ball.goto(0, 0)
+    #     bounceX()
+
+    # Paddle Collisions
+    if (ball.xcor() > 428 and (ball.ycor() < rPaddle.ycor()+65 and ball.ycor() > rPaddle.ycor()-65)):
+        bounceX()
+
+    if ball.xcor() < -428 and (ball.ycor() < lPaddle.ycor()+65 and ball.ycor() > lPaddle.ycor()-65):
+        bounceX()
+
+    # Ball Movement
+    ball.setx(ball.xcor() + ballx)
+    ball.sety(ball.ycor() + bally)
