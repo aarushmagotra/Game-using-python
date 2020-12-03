7facfeb625ef8d31f5d6516105279b9d81020706
muteenk <kundangarmuteen@protonmail.com>
2020-12-03 19:23:07 +0530

    made starting screen

--- pong.py
+++ pong.py
@@ -4,9 +4,46 @@
 ###################
 
 import turtle as tr
+import tkinter as tk
 import time
 import random as r
 
+
+
+######################
+# IMPORTANT VARIABLES
+######################
+
+
+pause = 0
+
+
+
+############################
+#   START MENU FOR THE GAME
+############################
+
+start_screen = tk.Tk(className=" PONG MENU")
+# start_screen.title("PONG MENU ;)")
+start_screen.configure(bg="grey")
+start_screen.geometry("600x400")
+start_screen.minsize(600, 400)
+start_screen.maxsize(600, 400)
+
+head = tk.Label(start_screen, text = "PONG MENU", bg="grey", pady=50) 
+head.config(font =("Courier", 35)) 
+head.pack()
+
+button1 = tk.Button(start_screen, text='Play', width=40, height=2, bg='white', fg='#000000', activebackground='#66ff66', font=25)
+button1.pack()
+
+button2 = tk.Button(start_screen, text='Difficulty', width=40, height=2, bg='white', fg='#000000', activebackground='#ff9933', font=25)
+button2.pack()
+
+button3 = tk.Button(start_screen, text='Exit', width=40, height=2, bg='white', fg='#000000', activebackground='#ff355e', font=25)
+button3.pack()
+
+start_screen.mainloop()
 
 
 ########################
@@ -17,7 +54,7 @@ import random as r
 win = tr.Screen()
 win.bgcolor("black")
 win.setup(height=1000, width=1200)
-win.title("Pong")
+win.title("PONG")
 win.tracer(0)
 
 # For Full Screen
@@ -60,13 +97,56 @@ cCircle.circle(100)
 
 
 ####################
+# PLAYERS INFO
+####################
+
+player1 = tr.Turtle()
+player1.hideturtle()
+player1.color("white")
+player1.penup()
+player1.goto(-450, 410)
+player1.write("Player1: 0", font=("Arial", 20, "italic"))
+player1_score = 0
+
+player2 = tr.Turtle()
+player2.hideturtle()
+player2.color("white")
+player2.penup()
+player2.goto(50, 410)
+player2.write("Player2: 0", font=("Arial", 20, "italic"))
+player2_score = 0
+
+
+#####################
+# CREDITS
+#####################
+
+credit = tr.Turtle()
+credit.hideturtle()
+credit.color("white")
+credit.penup()
+credit.goto(0, -450)
+credit.write("MADE BY~ MUTEEN, AARUSH, ARJUN", font=("Arial", 20, "italic"))
+
+
+#####################
+# PAUSE SCREEN
+#####################
+
+pause_txt = tr.Turtle()
+pause_txt.hideturtle()
+pause_txt.color("white")
+pause_txt.penup()
+pause_txt.goto(-168, -50)
+
+####################
 #   Components
 ####################
 
 # ball
 ball = tr.Turtle("circle")
 ball.shapesize(1.2, 1.2)
-ball_colour = ["violet", "indigo", "cyan", "green", "yellow", "#d35400", "red", "#7cfc00", "#008b8b"]
+ball_colour = ["#803790", "indigo", "cyan", "#66ff66", "#ffff66", "#ff9933", "#ff355e", "#7cfc00", "#ff3399" , "#50bfe6", "#c8c8cd"]
 prevColor = r.choice(ball_colour)
 ball.color(prevColor)
 ball.speed(1)
@@ -78,7 +158,7 @@ bally = r.choice(speed_lst)
 
 # left paddle
 lPaddle = tr.Turtle("square")
-lPaddle.color("blue")
+lPaddle.color("#50bfe6")
 lPaddle.shapesize(6, 0.3)
 lPaddle.speed(0)
 lPaddle.penup()
@@ -86,12 +166,11 @@ lPaddle.goto(-450, 0)
 
 # right paddle
 rPaddle = tr.Turtle("square")
-rPaddle.color("blue")
+rPaddle.color("#50bfe6")
 rPaddle.shapesize(6, 0.3)
 rPaddle.speed(0)
 rPaddle.penup()
 rPaddle.goto(450, 0)
-
 
 
 #######################
@@ -129,7 +208,10 @@ def leftDown():
 # Ball colour change on bouncing
 def colorChange():
     global ball_colour
+    global prevColor
     clr = r.choice(ball_colour)
+    while clr == prevColor:
+        clr = r.choice(ball_colour)
     ball.color(clr)
     return clr
 
@@ -138,8 +220,12 @@ def bounceX():
     global ballx
     ball.shapesize(0.8, 1.2)
     cColor = colorChange()
+    credit.undo()
+    credit.color(cColor)
+    credit.write("MADE BY~ MUTEEN, AARUSH, ARJUN", font=("Arial", 20, "italic"))
     ballx *= -1
     ball.shapesize(1.2, 1.2)
+
     return cColor
 
 
@@ -148,9 +234,24 @@ def bounceY():
     global bally
     ball.shapesize(1.2, 0.8)
     cColor = colorChange()
+    credit.undo()
+    credit.color(cColor)
+    credit.write("MADE BY~ MUTEEN, AARUSH, ARJUN", font=("Arial", 20, "italic"))
     bally *= -1
     ball.shapesize(1.2, 1.2)
     return cColor
+
+
+# Game Pause
+def pauseGame():
+    global pause
+   
+    if pause == 1:
+        pause -= 1
+        pause_txt.undo()
+    elif pause == 0:
+        pause += 1
+        pause_txt.write("PAUSED", font=("Arial", 60, "bold"))
 
 
 
@@ -163,6 +264,7 @@ win.onkeypress(rightUp, "Up")
 win.onkeypress(rightDown, "Down")
 win.onkeypress(leftUp, "w")
 win.onkeypress(leftDown, "s")
+win.onkeypress(pauseGame, "p")
 
 
 
@@ -170,10 +272,14 @@ win.onkeypress(leftDown, "s")
 # The main game loop
 #####################
 
-while True:
+while player1_score < 6 and player2_score < 6:
+
+    # to update the screen
     win.update()
 
-
+    # To Pause the main game loop
+    if pause == 1:
+        continue
 
     # Border Collisions
     ###################
@@ -193,14 +299,21 @@ while True:
     # Border Collision for right
     if ball.xcor() > 485:
         ball.goto(0, 0)
+        player1_score += 1
+        player1.undo()
+        player1.write("Player1: {0}".format(player1_score), font=("Arial", 20, "italic"))
         time.sleep(1)
         ballx = r.choice(speed_lst)
         bally = r.choice(speed_lst)
+
         
 
     # Border Collision for left
     if ball.xcor() < -485:
         ball.goto(0, 0)
+        player2_score += 1
+        player2.undo()
+        player2.write("Player2: {0}".format(player2_score), font=("Arial", 20, "italic"))
         time.sleep(1)
         ballx = r.choice(speed_lst)
         bally = r.choice(speed_lst)
@@ -214,6 +327,9 @@ while True:
     if ((ball.xcor() >= 438 and ball.xcor()<=439) and (ball.ycor() <= rPaddle.ycor()+75 and ball.ycor() >= rPaddle.ycor()-75)):
         nColor = bounceX()
         rPaddle.color(prevColor)
+        player2.undo()
+        player2.color(prevColor)
+        player2.write("Player2: {0}".format(player2_score), font=("Arial", 20, "italic"))
         prevColor = nColor
         
         
@@ -222,6 +338,9 @@ while True:
     if (ball.xcor() <= -438 and ball.xcor()>=-439) and (ball.ycor() <= lPaddle.ycor()+75 and ball.ycor() >= lPaddle.ycor()-75):
         nColor = bounceX()
         lPaddle.color(prevColor)
+        player1.undo()
+        player1.color(prevColor)
+        player1.write("Player1: {0}".format(player1_score), font=("Arial", 20, "italic"))
         prevColor = nColor
         
         
@@ -236,3 +355,12 @@ while True:
 
     # Ball movement Y-axis
     ball.sety(ball.ycor() + bally)
+
+
+end_screen = tk.Tk()
+end_screen.title("GAME OVER!!!")
+end_screen.configure(bg="grey")
+end_screen.geometry("600x400")
+end_screen.minsize(600, 400)
+end_screen.maxsize(600, 400)
+end_screen.mainloop()
