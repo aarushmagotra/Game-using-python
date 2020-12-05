498c2828e66bd252b52a8700199d5641788767d4
Aarush <aarushmagotra@protonmail.ch>
2020-12-05 22:38:44 +0530

    testing changes

--- pong.py
+++ pong.py
@@ -20,7 +20,7 @@ bally = r.choice(speed_lst)
 pause = 0
 start_var = 0
 
-def game():
+def game(p1="player1", p2="player2"):
     global ballx
     global bally
     global pause
@@ -29,11 +29,7 @@ def game():
     #####################
     # screen destruction
     #####################
-    if start_var == 0:
-        start_var += 1
-        start_screen.destroy()
-    else:
-        end_screen.destroy()      
+          
 
 
 
@@ -96,18 +92,19 @@ def game():
     player1.color("white")
     player1.penup()
     player1.goto(-450, 410)
-    player1.write("Player1: 0", font=("Arial", 20, "italic"))
+    player1.write("{}: 0".format(p1), font=("Arial", 20, "italic"))
     player1_score = 0
-    player1 = playerNames()[0]
-    print(player1)
+    
+    
 
     player2 = tr.Turtle()
     player2.hideturtle()
     player2.color("white")
     player2.penup()
     player2.goto(50, 410)
-    player2.write("Player2: 0", font=("Arial", 20, "italic"))
+    player2.write("{}: 0".format(p2), font=("Arial", 20, "italic"))
     player2_score = 0
+
 
 
     #####################
@@ -292,7 +289,7 @@ def game():
             ball.goto(0, 0)
             player1_score += 1
             player1.undo()
-            player1.write("Player1: {0}".format(player1_score), font=("Arial", 20, "italic"))
+            player1.write("{1}: {0}".format(player1_score, p1), font=("Arial", 20, "italic"))
             time.sleep(1)
             ballx = r.choice(speed_lst)
             bally = r.choice(speed_lst)
@@ -304,7 +301,7 @@ def game():
             ball.goto(0, 0)
             player2_score += 1
             player2.undo()
-            player2.write("Player2: {0}".format(player2_score), font=("Arial", 20, "italic"))
+            player2.write("{1}: {0}".format(player2_score, p2), font=("Arial", 20, "italic"))
             time.sleep(1)
             ballx = r.choice(speed_lst)
             bally = r.choice(speed_lst)
@@ -361,8 +358,73 @@ def quitGame():
 #   START MENU FOR THE GAME
 ############################
 
+
+#####################
+# PLAYER NAMES
+#####################
+
+# player names taker
+
+def playerNames():
+    player1_name = player1_Name.get()
+    player2_name = player2_Name.get()
+    name_screen.destroy()
+    if len(player1_name) != 0 and len(playe2_name) != 0:    
+        game(player1_name, player2_name)
+
+    elif len(player1_name) == 0 and len(player2_name) != 0:
+        game(p2=player2_name)
+    elif len(player2_name) == 0 and len(player1_name) != 0:
+        game(p1=player1_name)
+
+def names():
+    global start_var
+
+    if start_var == 0:
+        start_var += 1
+        start_screen.destroy()
+    else:
+        end_screen.destroy()
+
+    name_screen = tk.Tk()
+    name_screen.title("Enter Names:")
+    name_screen.configure(bg="grey")
+
+    name_screen.geometry("600x400")
+    name_screen.minsize(600, 400)
+    name_screen.maxsize(600, 400)
+
+
+    entry_text = tk.Label(text="Enter Names:", font=("Courier", 25), bg="grey")
+    entry_text.pack(pady=50, padx=150)
+
+    player1_var = tk.StringVar()
+    player2_var = tk.StringVar()
+
+    player_text1 = tk.Label(text="Enter player 1 name:", bg="grey", font=("Arial", 15, "italic"))
+    player_text1.pack()
+
+    player1_Name = tk.Entry(text="enter",width=40, bd=6, relief="flat", textvariable=player1_var)
+    player1_Name.pack(pady=10)
+
+    player_text2 = tk.Label(text="Enter player 2 name:", bg="grey", font=("Arial", 15, "italic"))
+    player_text2.pack()
+
+    player2_Name = tk.Entry(text="enter",width=40, bd=6, relief="flat", textvariable=player2_var)
+    player2_Name.pack(pady=10)
+
+    button_name = tk.Button(name_screen, text='Play', width=10, height=1, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), relief="flat", command=playerNames)
+    button_name.pack(pady=7)
+
+    foot_name = tk.Label(name_screen, text="Made By ~ Muteen, Aarush & Arjun", font=("Courier", 12 , "italic underline"), bg="grey")
+    foot_name.pack(side="bottom", anchor="e")
+
+    name_screen.mainloop()
+
+
+
+
 start_screen = tk.Tk(className=" PONG MENU")
-# start_screen.title("PONG MENU ;)")
 start_screen.configure(bg="grey")
 start_screen.geometry("600x400")
 start_screen.minsize(600, 400)
@@ -372,7 +434,7 @@ head_start = tk.Label(start_screen, text = "PONG MENU", bg="grey", pady=50)
 head_start.config(font =("Courier", 35)) 
 head_start.pack()
 
-button1_start = tk.Button(start_screen, text='Play', width=40, height=2, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), command = game, relief="flat")
+button1_start = tk.Button(start_screen, text='Play', width=40, height=2, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), command = names, relief="flat")
 button1_start.pack()
 
 button2_start = tk.Button(start_screen, text='Difficulty', width=40, height=2, bg='white', fg='#000000', activebackground='#ff9933', font=("arial", 12, "bold italic"), relief="flat")
@@ -386,55 +448,9 @@ foot_start.pack(side="bottom", anchor="e")
 
 start_screen.mainloop()
 
-#####################
-# PLAYER NAMES
-#####################
 
-# player names taker
 
-def playerNames():
-    player1_name = player1_Name.get()
-    player2_name = player2_Name.get()
-    return [player1_name, player2_name]
 
-name_screen = tk.Tk()
-name_screen.title("Enter Names")
-name_screen.configure(bg="grey")
-
-name_screen.geometry("600x400")
-name_screen.minsize(600, 400)
-name_screen.maxsize(600, 400)
-
-# text_frame = tk.Frame(name_screen)
-# text_frame.grid(row=20, column=10)
-
-entry_text = tk.Label(text="Enter Names:", font=("Courier", 25), bg="grey")
-entry_text.pack(pady=50, padx=150)
-
-player1_var = tk.StringVar()
-player2_var = tk.StringVar()
-
-player_text1 = tk.Label(text="Enter player 1 name:", bg="grey", font=("Arial", 15, "italic"))
-player_text1.pack()
-
-player1_Name = tk.Entry(text="enter",width=40, bd=6, relief="flat", textvariable=player1_var)
-player1_Name.pack(pady=10)
-
-player_text2 = tk.Label(text="Enter player 2 name:", bg="grey", font=("Arial", 15, "italic"))
-player_text2.pack()
-
-player2_Name = tk.Entry(text="enter",width=40, bd=6, relief="flat", textvariable=player2_var)
-player2_Name.pack(pady=10)
-
-button_name = tk.Button(name_screen, text='Play', width=10, height=1, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), relief="flat", command=playerNames)
-button_name.pack(pady=7)
-
-foot_name = tk.Label(name_screen, text="Made By ~ Muteen, Aarush & Arjun", font=("Courier", 12 , "italic underline"), bg="grey")
-foot_name.pack(side="bottom", anchor="e")
-
-name_screen.mainloop()
-
-print(playerNames()[0])
 
 
 
@@ -450,7 +466,7 @@ while True:
     head_end.config(font =("Courier", 35)) 
     head_end.pack()
 
-    button1_end = tk.Button(end_screen, text='Play Again', width=40, height=2, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), command = game, relief="flat")
+    button1_end = tk.Button(end_screen, text='Play Again', width=40, height=2, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), command = names, relief="flat")
     button1_end.pack()
 
     button2_end = tk.Button(end_screen, text='Difficulty', width=40, height=2, bg='white', fg='#000000', activebackground='#ff9933', font=("arial", 12, "bold italic"), relief="flat")
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 
 
-def hi():
-    name_screen.destroy()
+def playerNames():
+    player1_name = player1_Name.get()
+    player2_name = player2_Name.get()
+    
+    return(player1_name, player2_name)
+
 
 name_screen = tk.Tk()
 name_screen.title("Enter Names")
@@ -35,7 +39,7 @@ player2_Name = tk.Entry(text="enter",width=40, bd=6, relief="flat", textvariable
 player2_Name.pack(pady=10)
 
 
-button_name = tk.Button(name_screen, text='Play', width=10, height=1, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), relief="flat", command=hi)
+button_name = tk.Button(name_screen, text='Play', width=10, height=1, bg='white', fg='#000000', activebackground='#66ff66', font=("arial", 12, "bold italic"), relief="flat", command=playerNames)
 button_name.pack(pady=7)
 
 foot_name = tk.Label(name_screen, text="Made By ~ Muteen, Aarush & Arjun", font=("Courier", 12 , "italic underline"), bg="grey")
@@ -43,10 +47,6 @@ foot_name.pack(side="bottom", anchor="e")
 
 name_screen.mainloop()
 
-def playerNames():
-    player1_name = player1_Name.get()
-    player2_name = player2_Name.get()
-    
-    return(player1_name, player2_name)
+
 a = playerNames()
 print(a)
